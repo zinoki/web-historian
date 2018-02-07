d04d0e6719e4fd7973d036bc020c28ae492cfa0a
zinoki <zinouye@gmail.com>
2018-02-07 15:49:24 -0800

    loading index.html

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,27 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var http = require('http');
+
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  fs.readFile('/Users/student/code/hrsf89-web-historian/web/public/index.html', function(err, html) {
+    if (err) {
+      throw err;
+    }
+    res.writeHeader(200, {"Content-Type": "text/html"});
+    res.write(html);
+    res.end();
+  })
+  //res.end(archive.paths.siteAssets);
 };
+
+  // fs.readFile(archive.paths.siteAssets, function(err, html) {
+  //   if (err) {
+  //     throw err;
+  //   }
+  //   res.writeHeader(200, {"Content-Type": "text/html"});
+  //   res.write(html);
+  //   res.end();
+  // })
