06c041f4fec09dba87222a3750723b9247bec4d9
zinoki <zinouye@gmail.com>
2018-02-07 17:37:21 -0800

    worked on post handling

--- web/request-handler.js
+++ web/request-handler.js
@@ -2,26 +2,40 @@ var path = require('path');
 var archive = require('../helpers/archive-helpers');
 var fs = require('fs');
 var http = require('http');
-
+var headers = require('./http-helpers');
+var qs = require('querystring');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  fs.readFile('/Users/student/code/hrsf89-web-historian/web/public/index.html', function(err, html) {
-    if (err) {
-      throw err;
-    }
-    res.writeHeader(200, {"Content-Type": "text/html"});
-    res.write(html);
-    res.end();
-  })
-  //res.end(archive.paths.siteAssets);
-};
+  var url = req.url;
+  var method = req.method;
+  console.log(req.url, 'this is the url entered in the search bar')
 
-  // fs.readFile(archive.paths.siteAssets, function(err, html) {
-  //   if (err) {
-  //     throw err;
-  //   }
-  //   res.writeHeader(200, {"Content-Type": "text/html"});
-  //   res.write(html);
-  //   res.end();
-  // })
+  if (url === '/' && method === 'GET') {
+    fs.readFile(archive.paths.siteAssets, function(err, html) {
+      if (err) {
+        throw err;
+      }
+      res.writeHeader(200, {"Content-Type": "text/html"});
+      res.write(html);
+      res.end();
+    });
+} else if(method === 'POST'){
+  var body = ""
+  req.on('data', function(data) {
+    
+    //res.writeHeader(200, {"Content-Type": "text/html"});
+    //res.write(html);
+    body += data
+    
+  });
+  req.on('end', function() {
+    console.log(qs.parse(body));
+  });
+  res.writeHeader(200, {"Content-Type": "text/html"});
+  res.end();
+} else {
+    res.writeHeader(404, null);
+    res.end();
+  }
+};
